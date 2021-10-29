4c71a1e974828d27c7c38dcb0eab5c7ce071d435
Eric Silva <54083779+EricSilvaSt@users.noreply.github.com>
2021-10-28 22:36:29 -0300

    Entrega 47 - Katas2
    Finalmente efetuada a entrega depois de bater bastante a cabeça para entender.

--- src/katas2.js
+++ src/katas2.js
@@ -1,36 +1,93 @@
-// comece a criar a sua função add na linha abaixo
+// Tarefa 1 - Adição.
+// Obs.: arg = argumento.
 
-// descomente a linha seguinte para testar sua função
-// console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
+function add(arg1, arg2){
 
+    return arg1 + arg2
+}
+add(3, 5)
 
-// comece a criar a sua função multiply na linha abaixo
-
-
-// descomente a linha seguinte para testar sua função
-// console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
-
-
-// comece a criar a sua função power na linha abaixo
+console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
 
 
-// descomente a linha seguinte para testar sua função
-// console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
 
 
-// comece a criar a sua função factorial na linha abaixo
 
 
-// descomente a linha seguinte para testar sua função
-// console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
+// Tarefa 2 - Multiplicação.
+// Obs.: arg = argumento.
+
+function multiply(arg1, arg2){
+
+    let saida = 0
+    
+    for(let contador = 1; contador <= arg1; contador++) {
+    
+    saida = add(saida, arg2)
+    
+    }
+    return saida
+}
+multiply(4, 6)
+
+console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
+
+
+
+
+
+// Tarefa 3 - Potenciação.
+// Obs.: arg = argumento.
+
+function power(arg1, arg2){
+
+    let saida = 1
+
+    for(let contador = 1; contador <= arg2; contador++) 
+    
+    saida = multiply(saida, arg1)
+
+    return saida
+}
+power(3, 4)
+
+console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
+
+
+
+
+
+// Tarefa 4 - Fatorial
+// Obs.: arg = argumento.
+
+function factorial(arg1){
+
+    let saida = 1
+
+    for(let contador = 1; contador <= arg2; contador++){ 
+    
+    saida = multiply(saida, arg1)
+
+    }
+    return saida
+}
+factorial(5)
+
+console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
+
+
+
+
+
 
 
 /**
  * BONUS (aviso: o grau de dificuldade é bem maior !!!)
  */
 
-// crie a função fibonacci
+// Tarefa 5 - Fibonacci.
 
+// Infelizmente não tive cabeça para fazer o bônus, mas adoraria que alguém pudesse me ajudar com isso, em breve.
 
 // descomente a linha seguinte para testar sua função
 // console.assert(fibonacci(8) === 13, 'A função fibonacci não está funcionando como esperado');
